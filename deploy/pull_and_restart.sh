8578e28f9bdec7d5283ec5b95c6660d1e9019b8b
Seagullie <59409233+Seagullie@users.noreply.github.com>
2024-05-21 19:47:54 +0300

    skip lib check

--- deploy/pull_and_restart.sh
+++ deploy/pull_and_restart.sh
@@ -2,5 +2,5 @@ forever stop dist/runBot.js
 git pull
 npm install
 npm install git+https://github.com/Seagullie/telegram-bot-framework.git
-tsc .
+npx tsc -p ../tsconfig.json
 forever start dist/runBot.js
