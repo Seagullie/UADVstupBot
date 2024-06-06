564cce14b8efc6a0e163ca95ce0693bd8e338aa5
Seagullie <59409233+Seagullie@users.noreply.github.com>
2024-06-06 13:52:38 +0300

    chore: Update dependencies and fix deployment script

--- deploy/pull_and_restart.sh
+++ deploy/pull_and_restart.sh
@@ -3,5 +3,5 @@ git pull
 npm install
 npm uninstall telegram-bot-framework
 npm install git+https://github.com/Seagullie/telegram-bot-framework.git
-npx tsc -p ../tsconfig.prod.json
-forever start dist/runBot.js -o UADVstupBot.log
+npx tsc -p tsconfig.prod.json
+forever start dist/runBot.js
